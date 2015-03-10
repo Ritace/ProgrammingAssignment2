aa027f2918d52698995b7175d192f3dcb2b7fcd8
ritacethapa <rit.ace.thapa@gmail.com>
2015-03-10 13:27:04 +0545

    Added new code in cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,41 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+## set matrix, get the determinant, compute the inverse, check if set
+  y <- x
+  inv <- NULL
+  setMatrix<-function(x) y <<- x
+  getMatrix<-function() y
+  getDet<-function() c(nrow(y),ncol(y))
+  findInverse<-function(){
+    det<-getDet()
+    if (det[1]==det[2]){
+      inv <<- solve(y)
+      } else {
+      print("Not a square matrix")
+      inv <<- NULL
+    }
+  }
+  getInverse<-function() inv
+  list(setMatrix=setMatrix,getDet=getDet,getMatrix=getMatrix,findInverse=findInverse,getInverse=getInverse)
 }
-
 
 ## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  ## see if inverse if found
+  if(is.null(x$getInverse())){
+    print("here")
+    x$findInverse()
+    return(x$getInverse())
+  } else {
+    print("using cache")
+    return(x$getInverse())
+  }
 }
+sample_array = array(c(1,2,3,4),dim = c(2,2))
+
+cache_matrix<-makeCacheMatrix(sample_array)
+cache_matrix$findInverse()
+print(cacheSolve(cache_matrix))
+
